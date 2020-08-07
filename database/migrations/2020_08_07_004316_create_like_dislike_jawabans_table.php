ab23beae26b5f9f51013d7a1f589aba4a543b718
Yepi Septiana <yepi.septiana@smartfren.com>
2020-08-07 07:52:56 +0700

    Tugas Migration Selesai

---
+++ database/migrations/2020_08_07_004316_create_like_dislike_jawabans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikeDislikeJawabansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like_dislike_jawabans', function (Blueprint $table) {
+            $table->unsignedBigInteger('jawaban_id');
+            $table->foreign('jawaban_id')->references('id')->on('jawabans');
+            $table->unsignedBigInteger('profil_id');
+            $table->foreign('profil_id')->references('id')->on('profiles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('like_dislike_jawabans');
+    }
+}
